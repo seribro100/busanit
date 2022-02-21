19639a96be4c9f5b5486085bc1677bd295add354
seribro100 <seribro100@gmail.com>
2022-02-21 16:26:28 +0900

    0221-2

--- python/file1.py
+++ python/file1.py
@@ -1,5 +1,24 @@
-f = open("python/새파일.txt", 'r')
-lines = f.readlines()
-for line in lines:
-    print(line, end='')
-f.close()  
+#f = open("python/새파일.txt", 'r')
+#lines = f.readlines()
+#for line in lines:
+#    print(line)
+#f.close() 
+
+
+#f = open("python/새파일.txt", 'r')
+#lines = f.readlines()
+#for line in lines:
+#    print(line, end='')
+#f.close() 
+
+#f = open("python/새파일.txt", 'r')
+#data = f.read()
+#print(data)
+#f.close()
+
+import sys
+
+args = sys.argv[1:]
+for i in args:
+    print(i)
+    
