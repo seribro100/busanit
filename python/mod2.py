9354a17b3741e542dd5457dbfea68abdc3d6bff3
seribro100 <seribro100@gmail.com>
2022-03-14 12:43:45 +0900

    0314-2

--- python/mod2.py
+++ python/mod2.py
@@ -1,4 +1,18 @@
-import mod1
+# import mod1
+# from mod1 import add, sub
 
-print(mod1.add(3,4))
-print(mod1.sub(4,2))
+# print(mod1.add(3,4))
+# print(mod1.sub(4,2))
+
+
+# print(add(3,4))
+# print(sub(4,2))
+
+PI = 3.141592
+
+class Math:
+    def solv(self, r):
+        return PI * (r ** 2)
+    
+def add(a, b):
+    return a + b    
